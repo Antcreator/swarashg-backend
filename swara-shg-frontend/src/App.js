2cb8f4ddcacce63828ed114e8b918cb38924014b
Antcreator <antonywekesa8@gmail.com>
2026-05-12 08:41:38 +0300

    fix: default route

--- swara-shg-frontend/src/App.js
+++ swara-shg-frontend/src/App.js
@@ -33,6 +33,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/"                element={<Login />} />
         <Route path="/login"           element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
 
